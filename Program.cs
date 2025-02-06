2d61b9e9062dd3af7727531c05da6c19706c9cb7
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-02-06 23:00:20 +0700

    Улучшен вывод, функции вынесены за пределы Main

--- Program.cs
+++ Program.cs
@@ -13,72 +13,72 @@ namespace ConsoleApp
         {
             public numberLessZero(string message) : base(message) { }
         }
+        static bool firstProgram()
+        {
+            long number;
+            int powerNum;
+
+            while (true)
+            {
+                Console.Write("Введите число: ");
+                try
+                {
+                    number = Convert.ToInt64(Console.ReadLine());
+                    if (number == 0)
+                    {
+                        throw new numberIsZero("Число должно быть не равно 0");
+                    }
+                    break;
+                }
+                catch (numberIsZero e)
+                {
+                    Console.WriteLine($"Ошибка! {e.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка! Попробуйте ввести целое число \n{ex.Message}");
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("Введите степень возводимого числа: ");
+                try
+                {
+                    powerNum = Convert.ToInt32(Console.ReadLine());
+                    if (powerNum <= 0)
+                    {
+                        throw new numberLessZero("Число должно быть больше нуля");
+                    }
+                    break;
+                }
+                catch (numberLessZero e)
+                {
+                    Console.WriteLine($"Ошибка! {e.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка! Попробуйте ввести целое число \n{ex.Message}");
+                }
+            }
+
+            Console.WriteLine($"Вывод: {number}^{powerNum} = {MultipleNumber(number, powerNum)}");
+            return true;
+        }
+
+        static long MultipleNumber(long number, int powerNum)
+        {
+            long fixNumber = number;
+            for (int indexNum = 1; indexNum < powerNum; ++indexNum)
+            {
+                number *= fixNumber;
+            }
+            return number;
+        }
 
         static void Main(string[] args)
         {
-            long MultipleNumber(long number, int powerNum)
-            {
-                long fixNumber = number;
-                for (int indexNum = 1; indexNum < powerNum; ++indexNum)
-                {
-                    number *= fixNumber;
-                }
-                return number;
-            }
-
-            bool firstProgram()
-            {
-
-                long number;
-                int powerNum;
-
-                while (true)
-                {
-                    Console.Write("Введите число: ");
-                    try
-                    {
-                        number = Convert.ToInt64(Console.ReadLine());
-                        if (number == 0)
-                        {
-                            throw new numberIsZero("Число должно быть не равно 0");
-                        }
-                        break;
-                    }
-                    catch (numberIsZero e)
-                    {
-                        Console.WriteLine($"Ошибка! {e.Message}");
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Ошибка! Попробуйте ввести целое число \n{ex.Message}");
-                    }
-                }
-
-                while (true)
-                {
-                    Console.Write("Введите степень возводимого числа: ");
-                    try
-                    {
-                        powerNum = Convert.ToInt32(Console.ReadLine());
-                        if (powerNum <= 0)
-                        {
-                            throw new numberLessZero("Число должно быть больше нуля");
-                        }
-                        break;
-                    }
-                    catch (numberLessZero e)
-                    {
-                        Console.WriteLine($"Ошибка! {e.Message}");
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Ошибка! Попробуйте ввести целое число \n{ex.Message}");
-                    }
-                }
-
-                Console.WriteLine($"Вывод: {MultipleNumber(number, powerNum)}");
-                return true;
-            }
+            firstProgram();
         }
     }
 }
